4620b0d35eb3aee096c6e4265861f4f84b561b2e
Omar Sobh <omarsobh@Omars-MacBook-Pro.local>
2018-04-19 11:54:12 -0700

    starting to add in all educational modules

---
+++ python/arrays/intro.py
@@ -0,0 +1,16 @@
+# Setup a variable with a simple list
+a = ["item01", "item02", "item03"]
+
+# loop over each item in the list
+for value in a:
+    print(value)
+
+#This would print:
+#item01
+#item02
+#item03
+
+#Similar output different style
+#understanding the range function
+for i in range(len(a)):
+    print (a[i])
