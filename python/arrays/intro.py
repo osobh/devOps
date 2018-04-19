0a4a86b6b0efb6decb21c1090739f3d0dbd4a036
Omar Sobh <om.sobh@gmail.com>
2018-04-19 12:58:21 -0700

    completed arrays

--- python/arrays/intro.py
+++ python/arrays/intro.py
@@ -1,4 +1,4 @@
-# Setup a variable with a simple list
+#Setup a variable with a simple list
 a = ["item01", "item02", "item03"]
 
 # loop over each item in the list
@@ -16,11 +16,44 @@ for value in a:
 for i in range(len(a)):
     print (a[i])
 
-
-#--------------------------------------------------------------
-Practice Problems
-#--------------------------------------------------------------
-# Problem01 - Initialize a new list called a_list
+# --------------------------------------------------------------
+# Practice Problems
+# --------------------------------------------------------------
+Problem01 - Initialize a new list called a_list
 a_list = ["item01", "item02", "item03"]
 print("a_list is: ")
 print(a_list)
+
+# Change the second element of b_list
+b_list = [0,0,0]
+b_list[1]=1
+print("b_list is: ")
+print(b_list)
+
+c_list = ["item01","item02","item03"]
+for element in c_list:
+    print c_list
+
+# Iterate over each element in d_list and print it using the "for i in range" syntax
+d_list = ["item01","item02","item03"]
+for i in range(len(d_list)):
+    print d_list[i]
+
+# Find the sum of e_list
+e_list = [32,55,710,1]
+total = 0
+for i in range(len(e_list)):
+     total += e_list[i]
+     print total
+
+# Sample interview question #1
+Given the list [1,3,4,5,0,2] , return the second largest number
+masterarray = [1,3,4,5,0,2]
+largest = 0
+secondlargest = 0
+for i in range(len(masterarray)):
+    if masterarray[i] > largest:
+        secondlargest = largest
+        largest = masterarray[i]
+        print largest
+        print secondlargest
